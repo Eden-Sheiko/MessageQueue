1d4b2e612804975eaf59826124c26d8e48b89e31
Eden Sheiko <edensheiko@gmail.com>
2025-04-13 23:51:58 +0300

    first commit

---
+++ Inc/MessageQueue.h
@@ -0,0 +1,9 @@
+#ifndef MESSAGEQUEUE_H
+#define MESSAGEQUEUE_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct z_message_queue_s z_message_queue_t;
+
+#endif //MESSAGEQUEUE_H
---
+++ Src/MessageQueue.c
@@ -0,0 +1,6 @@
+#include "../Inc/MessageQueue.h"
+
+typedef struct z_message_queue_s{
+
+
+}z_message_queue_t;
